1a5ec267c3ecebf4e2ab3c9e723550d3d98e6943
Biswa <barikbiswa143@gmail.com>
2023-03-21 21:17:35 +0530

    Reducing in the cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
+import Cart from "./components/Cart/Cart";
+import { useState } from "react";
+import CartProvider from "./store/CartProvider.js";
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+  const showCartHandler = () => {
+    setCartShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartShown(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      {cartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
